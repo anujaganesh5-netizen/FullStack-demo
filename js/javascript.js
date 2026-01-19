2517167914d835e4227d6578f3a6cf2cd6315684
anujaganesh5-netizen <anujaganesh5@gmail.com>
2026-01-19 16:34:56 +0530

    first commit

---
+++ js/javascript.js
@@ -0,0 +1,43 @@
+ 
+     const slider = document.querySelector(".slider");
+      const dots = document.querySelectorAll(".dot");
+      const prevBtn = document.querySelector(".arrow.left");
+      const nextBtn = document.querySelector(".arrow.right");
+
+      let index = 0;
+      const slideWidth = 330; // image width + gap
+
+      function updateSlider() {
+        slider.style.transform = `translateX(${-index * slideWidth}px)`;
+
+        dots.forEach((dot) => dot.classList.remove("active"));
+        dots[index].classList.add("active");
+      }
+
+      nextBtn.addEventListener("click", () => {
+        index++;
+        if (index >= dots.length) index = 0;
+        updateSlider();
+      });
+
+      prevBtn.addEventListener("click", () => {
+        index--;
+        if (index < 0) index = dots.length - 1;
+        updateSlider();
+      });
+
+      dots.forEach((dot, i) => {
+        dot.addEventListener("click", () => {
+          index = i;
+          updateSlider();
+        });
+      });
+
+      setInterval(() => {
+        index++;
+        if (index >= dots.length) index = 0;
+        updateSlider();
+      }, 2500);
+ 
+
+
